9be0992b09e1faa4834a134bcef4163b582cefa1
Nimitt <gorasianimitt08@gmail.com>
2021-01-16 16:58:52 +0530

    New change

--- DigiPayWalletRevamp/scripts/DDL_Script.sql
+++ DigiPayWalletRevamp/scripts/DDL_Script.sql
@@ -1,3 +1,5 @@
 select * from persons;
 
 insert into persons values (1,'Doe','John','Nare Galle','Yeddappo','Chennai');
+
+insert into persons values (2,'Meow','The Great','Jingalala','Huii','USA');
