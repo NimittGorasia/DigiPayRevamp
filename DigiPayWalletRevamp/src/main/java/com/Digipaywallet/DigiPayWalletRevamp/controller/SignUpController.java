e7ec437c6251f18b37b7b2061da8a118f87798b3
Parthd9 <parthdevmurari9@gmail.com>
2021-01-18 20:59:13 +0530

    Connecting to angular.

--- DigiPayWalletRevamp/src/main/java/com/Digipaywallet/DigiPayWalletRevamp/controller/SignUpController.java
+++ DigiPayWalletRevamp/src/main/java/com/Digipaywallet/DigiPayWalletRevamp/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.Digipaywallet.DigiPayWalletRevamp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Digipaywallet.DigiPayWalletRevamp.entity.GenUsers;
 
 @RestController
-@RequestMapping("/signUp")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/signUp")
 public class SignUpController {
 
 	@PostMapping("/demoDetails")
-	public String demoDetails(@RequestBody GenUsers genUsers) {
+	public GenUsers demoDetails(@RequestBody GenUsers genUsers) {
+		GenUsers res = new GenUsers();
+		System.out.println(genUsers.getFirstName());
 		String result = "Welcome";
 		if(null != genUsers) {
 			result = result+genUsers.getFirstName();
+			res.setFirstName("Welcome "+genUsers.getFirstName());
 		}
-		return result;
+		return res;
 	}
 	
 }
