128c5f849c90a5bf25418c7b225b2a55566c7a01
NimittGorasia <32828601+NimittGorasia@users.noreply.github.com>
2021-02-09 23:23:53 +0530

    Jwt Expiration time

--- DigiPayWalletRevamp/src/main/java/com/Digipaywallet/DigiPayWalletRevamp/vo/JwtResponse.java
+++ DigiPayWalletRevamp/src/main/java/com/Digipaywallet/DigiPayWalletRevamp/vo/JwtResponse.java
@@ -6,13 +6,20 @@ public class JwtResponse implements Serializable {
 
 	private static final long serialVersionUID = -8091879091924046844L;
 	private final String jwttoken;
+	
+	private final String expDate;
 
-	public JwtResponse(String jwttoken) {
+	public JwtResponse(String jwttoken,String expDate) {
 		this.jwttoken = jwttoken;
+		this.expDate = expDate; 
 	}
 
 	public String getToken() {
 		return this.jwttoken;
+	}
+
+	public String getExpDate() {
+		return expDate;
 	}
 }
 
