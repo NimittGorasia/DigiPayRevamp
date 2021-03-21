6955ebad5b678cc391b72673c62d244bdbb09fde
NimittGorasia <32828601+NimittGorasia@users.noreply.github.com>
2021-03-21 22:33:59 +0530

    PaymentsVO
    PaymentsVO

---
+++ DigiPayWalletRevamp/src/main/java/com/Digipaywallet/DigiPayWalletRevamp/vo/PaymentsVO.java
@@ -0,0 +1,14 @@
+package com.Digipaywallet.DigiPayWalletRevamp.vo;
+
+public class PaymentsVO {
+ 
+	private String sum;
+
+	public String getSum() {
+		return sum;
+	}
+
+	public void setSum(String sum) {
+		this.sum = sum;
+	}
+}
